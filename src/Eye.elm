7a2c28005a6be7aeafa62be8329881de74346e9e
Pekka Neuvo <pnewo@Pekkas-MacBook-Pro.local>
2015-12-14 21:40:18 +0200

    simpler and better looking eye movement

--- src/Eye.elm
+++ src/Eye.elm
@@ -29,51 +29,28 @@ view (w, h) (x, y) =
   let
     halfW = toFloat w / 2
     halfH = toFloat h / 2
-    mouseX' = toFloat x / halfW - 1
-    mouseY' = 1 - toFloat y / halfH
-    (dx,dy) =
-      (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
+    dx = toFloat x - toFloat w / 2
+    dy = toFloat h / 2 - toFloat y
     eyeRadius = 50.0
-    irisRadius = eyeRadius * 3 / 5
-    pupilRadius = irisRadius / 2
-    irisX = mouseX' * (eyeRadius - irisRadius)
-    irisY = mouseY' * (eyeRadius - irisRadius)
-    pupilX = mouseX' * (eyeRadius - pupilRadius)
-    pupilY = mouseY' * (eyeRadius - pupilRadius)
+    irisDiameter = eyeRadius * 6 / 5
+    pupilDiameter = irisDiameter / 2
     eyeAngle = atan2 dy dx
-    eyeAngle2 = atan2 dx dy
-    corner = atan2 halfH halfW
-    otherCorner = pi - corner
-    topBottom = if  (abs (atan2 dy dx) > corner && abs (atan2 dy dx) < otherCorner)
-      then True
-      else False
     distance = sqrt (dy ^ 2 + dx ^ 2)
-    maxDistance = if topBottom then
-        abs (halfH / cos ( abs eyeAngle2 ))
-      else
-        abs (halfW / cos ( abs eyeAngle ))
-    distanceCoef = distance / maxDistance
+    maxDistance = (min halfH halfW) * 0.8
+    distanceCoef = clamp 0 1 (sqrt (distance / maxDistance))
   in
     collage w h
       [ group [
-          oval (irisRadius * 2 - 15 * distanceCoef) (irisRadius * 2)
+          oval (irisDiameter - 15 * distanceCoef) (irisDiameter)
             |> filled (rgb 107 176 71)
             |> move ((27 * distanceCoef), 0)
-        , oval (pupilRadius * 2 - 8 * distanceCoef) (pupilRadius * 2)
+        , oval (pupilDiameter - 8 * distanceCoef) (pupilDiameter)
             |> filled (rgb 50 50 50)
             |> move ((35 * distanceCoef), 0)
         ]
         |> rotate eyeAngle
       , eyeGlare
       , eyeBall eyeRadius
-      , toForm (show
-          { distance = distance
-          , eyeAngle = eyeAngle
-          , eyeAngle2 = eyeAngle2
-          , maxDistance = maxDistance
-          , distanceCoef = distanceCoef
-          }
-        )
       ]
 
 -- SIGNALS
